4b339b75294757fd4a901fa09878d4d288d5d83f
Nate Castor <nathancastor@gmail.com>
2016-02-01 14:58:46 -0800

    the number of ducks is based on the level now

--- app.js
+++ app.js
@@ -3,34 +3,40 @@ var imgObj = null;
 var animate ;
 var $sky = $('.sky')
 
+var game = {
+  currentPlayer: '',
+  playerOne: '',
+  playerTwo: '',
+  init: '',
+  round: [1,2,3],
+  level: 1,
+  winner: ''
+}
+
 function start() {
-  Duck()
-  Duck()
-  Duck()
+  var numDucks = game.level + 2
+  for (var i = 0; i < numDucks; i++) {
+    new Duck()
+  }
 }
 
 function Duck(){
   this.id = $('.duck').length
   $sky.append('<img class="duck" id="duck-' +this.id+ '" ' + 'src="images/duck_hunt_bird.GIF" />')
   this.selector = $('#duck-' + this.id)
-   this.selector.css({
-     top: Math.floor(Math.random() * 400) + 'px',
-     position: 'relative',
-     left: '0px',
-     height: '55px'
-   })
-   this.selector.animate({
-     left: "880px"
-   }, ((Math.random() * 2000) + 4000), function () {
-     $(this).remove()
-   })
-   this.selector.click(function () {
-     console.log("BOOM!");
-     $(this).attr('src','images/bam-md.png')
-   })
+  this.selector.css({
+  top: Math.floor(Math.random() * 400) + 'px',
+  position: 'relative',
+  left: '0px',
+  height: '55px'
+  })
+  this.selector.animate({
+   left: "880px"
+  }, ((Math.random() * 2000) + 4000), function () {
+   $(this).remove()
+  })
+  this.selector.click(function () {
+   console.log("BOOM!");
+   $(this).attr('src','images/bam-md.png')
+  })
 }
-
-//start button does start function which calls a round function which happens 3 times per level
-//the round function calls a number of Duck based on the level (two for level one)
-// also the speed in moveDuck goes up based on level.
-// The ducks then
