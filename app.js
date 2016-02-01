a09cb08e34275cbf36b7ba462026f94039812c47
Nate Castor <nathancastor@gmail.com>
2016-02-01 14:10:11 -0800

    three ducks at different speeds

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 
 var imgObj = null;
 var animate ;
-var sky = document.querySelector('.sky')
-var counterOne = 0;
+var $sky = $('.sky')
+
 function start() {
-  createDuck()
-  //createDuck()
+  Duck()
+  Duck()
+  Duck()
 }
-function createDuck(){
-  sky.innerHTML ='<img id="myImage' +counterOne+ '" ' + 'src="images/duck_hunt_bird.GIF" />'
-  counterOne++
-  var duck = 'myImage' + counterOne.toString()
-   imgObj = document.getElementById(duck);
-   imgObj.style.top = Math.floor(Math.random() * 10)
-   imgObj.style.position= 'relative';
-   imgObj.style.left = '0px';
-   moveRight()
+
+function Duck(){
+  this.id = $('.duck').length
+  $sky.append('<img class="duck" id="duck-' +this.id+ '" ' + 'src="images/duck_hunt_bird.GIF" />')
+  this.selector = $('#duck-' + this.id)
+   this.selector.css({
+     top: Math.floor(Math.random() * 400) + 'px',
+     position: 'relative',
+     left: '0px',
+     height: '55px'
+   })
+   this.selector.animate({
+     left: "880px"
+   }, ((Math.random() * 2000) + 4000), function () {
+     $(this).remove()
+   })
+   this.selector.click(function () {
+     console.log("BOOM!");
+     $(this).attr('src','images/bam-md.png')
+   })
 }
-function moveRight(){
-  imgObj.style.left = parseInt(imgObj.style.left) + 8 + 'px';
-  animate = setTimeout(moveRight,20); // call moveRight in 20msec
-}
-function stop(){
-   clearTimeout(animate);
-   imgObj.style.left = '0px';
-}
+
 //start button does start function which calls a round function which happens 3 times per level
-//the round function calls a number of createDuck based on the level (two for level one)
+//the round function calls a number of Duck based on the level (two for level one)
 // also the speed in moveDuck goes up based on level.
 // The ducks then
-
-// I need a random height variable
