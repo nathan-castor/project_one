322b577e8715b0725c6fabb7ef548b24c7ee5d82
Nate Castor <nathancastor@gmail.com>
2016-02-02 13:20:13 -0800

    broke something but cleaned it all up nicely

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var $sky = $('.sky')
 var bamPic = 'images/BAM.png'
 var bamPic2 = 'images/bam-md.png'
 
+// Add click event everywhere in body that makes a gun shot sound
+//$('body').click()
+// Later Add a click event in the footer that reloads your gun
+// $('footer').click()
+
 var game = {
   playerOne: {
     score: 0,
@@ -18,15 +23,16 @@ var game = {
     level: 1,
     round: 1,
   },
-  //round: 1,
-  //level: 1,
   winner: '',
   escapeSpeed: 3500,
   ducksThisRound: 0
 }
+
+// start the game with player One
 game.currentPlayer = game.playerOne
 
-function start() { //startROUND
+// ********* Start the Round ********************************
+function startRound() { //startROUND
   var numDucks = game.currentPlayer.level + 2
 
   for (var j = 0; j < numDucks; j+=1) { //making the ducks based on current level
@@ -36,37 +42,7 @@ function start() { //startROUND
   }
 }
 
-function checkEndofRound() {
-  if (game.ducksThisRound == 0) { // checking if round is over
-    console.log("It's the end of round");
-    if (checkEndofGame()) {  //checking for a loss
-      window.alert("game over your score: " + game.currentPlayer.score)
-    }else { // iterating to next round
-      console.log("starting next round");
-      if (game.currentPlayer.round < 3) {
-        game.currentPlayer.round += 1
-        console.log('start round  ' + game.currentPlayer.round)
-        start()
-      }else {
-        window.alert("You passed this Level");
-        game.currentPlayer.misses = 0
-        game.currentPlayer.round = 1
-        game.currentPlayer.level += 1
-        // Add delay
-        setTimeout(start, 3000)
-      }
-    }//end else
-  }else{
-    console.log("There's still ducks");
-  }//end check for no ducks
-}//end function
-function checkEndofGame() {
-  return game.currentPlayer.misses >= 3
-}
-
-// Add click event everywhere in body that makes a gun shot sound
-//$('body').click()
-
+// *********************Duck Constructor**********************
 // Create ducks with all the functionality built in
 function Duck(){
   this.id = $('.duck').length
@@ -92,7 +68,7 @@ function Duck(){
      console.log('misses ' + game.currentPlayer.misses);
      game.ducksThisRound -= 1
      console.log('ducks this round ' + game.ducksThisRound);
-     checkEndofRound()
+     checkEOR()
    })
   }else { // start duck on the right
       this.selector.css({
@@ -106,7 +82,7 @@ function Duck(){
        $(this).remove()
        game.currentPlayer.misses +=1
        game.ducksThisRound -= 1
-       checkEndofRound()
+       checkEOR()
       })
     }
 
@@ -122,6 +98,92 @@ function Duck(){
     console.log('ducks this round ' + game.ducksThisRound);
     // add to player score
     game.currentPlayer.score++
-    checkEndofRound()
+    checkEOR()
   })
 }//END of DUCK CONSTRUCTOR
+// ********************* End Duck Constructor**********************
+
+
+// ************************************************************************
+// --------- the game logic ----------
+// ************************************************************************
+function checkEOR() {
+  if (eor()) { // checking if round is over
+    console.log("It's the end of round");
+    if (loss()) { // check if end of game for one player
+      if (eog()) { // check if game is over
+        whoWon()
+      }else { // if game is not over end game for current player and switch players
+        loss()
+      }
+    }else { // if no loss
+      if (eol()) { // if end of level switch players
+        nextLevel()
+      }else { // if not end of level go to next round
+        nextRound()
+      }
+    }
+  }else{ // if not end of round
+    console.log("There's still ducks");
+  }//end check for no ducks
+}//end function
+
+// ************************************************************************
+// --------- Below are the functions that make up the game logic ----------
+// ************************************************************************
+
+function eor() {
+  return game.ducksThisRound == 0
+}
+function loss() {
+  return game.currentPlayer.misses >= 3
+}
+function eol() {
+  return game.currentPlayer.round >= 3
+}
+function eog() {
+  return (game.playerOne.misses >= 3 && game.playerTwo.misses >= 3)
+}
+function loss() {
+  console.log("end of game for "+ game.currentPlayer);
+  window.alert(game.currentPlayer + " You're done!")
+  switchPlayers()
+  window.alert(game.currentPlayer + " Get READY")
+  setTimeout(startRound, 3000)
+}
+function switchPlayers() {
+  switch (game.currentPlayer) {
+    case game.currentPlayer = game.playerOne:
+      game.currentPlayer = game.playerTwo;
+      break;
+    case game.currentPlayer = game.playerTwo:
+      game.currentPlayer = game.playerOne;
+      break;
+    default:
+      game.playerOne;
+  }
+}
+function whoWon() {
+  if (game.playerOne.score > game.playerTwo.score) {
+    window.alert("game over " +game.playerOne+"wins! \nplayer one score: " + game.playerOne.score + "\nplayer two score: "+game.playerTwo.score)
+  }else if (game.playerOne.score > game.playerTwo.score) {
+    window.alert("game over " +game.playerTwo+"wins! \nplayer one score: " + game.playerOne.score + "\nplayer two score: "+game.playerTwo.score)
+  }else {
+    window.alert("Tie Game! \nplayer one score: " + game.playerOne.score + "\nplayer two score: "+game.playerTwo.score)
+  }
+}
+function nextRound() {
+  game.currentPlayer.round += 1
+  console.log(game.currentPlayer + 'start round:  ' + game.currentPlayer.round)
+  startRound()
+}
+function nextLevel() {
+    window.alert(game.currentPlayer+ " You passed this Level. Next player!");
+    game.currentPlayer.misses = 0
+    game.currentPlayer.round = 1
+    game.currentPlayer.level += 1
+    // switch to other player
+    switchPlayers()
+    // Add delay
+    setTimeout(start, 3000)
+}
